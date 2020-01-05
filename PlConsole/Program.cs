98d84702f838169b9b0d04eafb5718aa9b4a1f89
avrumi rosenberg <avrumi96@gmail.com>
2020-01-05 22:10:16 +0200

    hi

--- PlConsole/Program.cs
+++ PlConsole/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DalApi;
+using DO;
 
 namespace PlConsole
 {
     class Program
     {
+        static IDal dal = DalFactory.GetDal();
         static void Main(string[] args)
         {
+            foreach (var branch in dal.GetBranches())
+                Console.WriteLine("Branch {1} of {0} in {2}", branch.BankName, branch.BranchNumber, branch.BranchCity);
         }
     }
 }
