92321cb8ff84f9e1db33d2d9d4d66a53ed61dd0c
avrumi rosenberg <avrumi96@gmail.com>
2020-01-10 15:55:23 +0200

    hi jjj

---
+++ BL/BlFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi
+{
+    public static class BlFactory
+    {
+        public static IBl GetBL()
+        {
+            return new BL.BLImp();
+        }
+    }
+}
+
